29bd12ffe1e08fdf224979431f90afefa0a244d1
xiyuhan994 <15194526412@163.com>
2022-04-14 13:45:23 +0800

    update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -329,8 +329,8 @@
                 <div class="footer-right-item-title">LEARN</div>
                 <div class="footer-right-item-tab">
                   <a href="https://medium.com/marcopolo-protocol" target="_blank"><img src="../assets/footer/one.png"/>Medium</a>
-                  <a href="https://twitter.com/MarcoPoloGlobal" target="_blank"><img src="../assets/footer/two.png"/>Twitter</a>
-                  <a href="https://t.me/MAPprotocol" target="_blank"><img src="../assets/footer/three.png"/>Telegram</a>
+                  <a href="https://twitter.com/MarcoPoloGlobal" target="_blank"><img src="../assets/footer/three.png"/>Twitter</a>
+                  <a href="https://t.me/MAPprotocol" target="_blank"><img src="../assets/footer/two.png"/>Telegram</a>
                   <a href="https://open.kakao.com/o/gP9WduRb" target="_blank"><img src="../assets/footer/four.png"/>KakaoTalk</a>
                   <a href="https://blog.naver.com/marcopay"  target="_blank"><img src="../assets/footer/five.png"/>Blog</a>
                 </div>
