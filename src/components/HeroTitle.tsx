053826d7c9ae76124a2f73b372ae14de87c1bafa
Alpha <piotr@piotryordanov.com>
2022-10-17 19:44:20 +0400

    fix: hero title: change font. add blue. fix padding with top

--- src/components/HeroTitle.tsx
+++ src/components/HeroTitle.tsx
@@ -10,10 +10,10 @@ const HeroTitle = () => {
     <div className=''>
       <div className='mx-auto flex h-screen content-center items-center justify-center text-center font-primary'>
         <div className=''>
-          <div className='text-4xl font-bold text-blue-100 lg:text-6xl lg:leading-relaxed'>
+          <div className='text-4xl font-bold text-blue-100 lg:text-5xl lg:leading-relaxed'>
             Convert single chain dApps into Ominichain dApps
           </div>
-          <div className='my-4 text-xl font-bold leading-loose text-gray-300 lg:text-3xl'>
+          <div className='my-4 text-xl font-bold leading-loose text-gray-300 lg:text-2xl'>
             Built upon{' '}
             <div className='inline text-blue-300'> light-client </div> and
             <div className='inline text-blue-300'> zk-Technology </div>
@@ -24,32 +24,6 @@ const HeroTitle = () => {
               <Stake className='hover:pointer text-[14rem] transition-all duration-100 hover:scale-125 lg:text-[24rem]' />
             </Link>
           </div>
-        </div>
-      </div>
-
-      <div className='mx-auto flex h-screen content-center items-center justify-center border-y-2 text-center font-primary'>
-        <div className='flex flex-col'>
-          <h1 className='mb-8'>
-            Convert single chain dApps into Ominichain dApps
-          </h1>
-          <h3 className=''>Convert single chain dApps into Ominichain dApps</h3>
-          <Link href='/stake' className='mx-auto '>
-            <Stake className='hover:pointer text-[14rem] transition-all duration-100 hover:scale-125 lg:text-[24rem]' />
-          </Link>
-        </div>
-      </div>
-
-      <div className='mx-auto flex h-screen content-center items-center justify-center border-y-2 text-center font-primary'>
-        <div className='flex flex-col'>
-          <Title className='mb-8'>
-            Convert single chain dApps into Ominichain dApps
-          </Title>
-          <Title level={3}>
-            Convert single chain dApps into Ominichain dApps
-          </Title>
-          <Link href='/stake' className='mx-auto '>
-            <Stake className='hover:pointer text-[14rem] transition-all duration-100 hover:scale-125 lg:text-[24rem]' />
-          </Link>
         </div>
       </div>
     </div>
