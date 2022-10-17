c0f215c685c5c2807fa322bf3919ce13722c235c
Alpha <piotr@piotryordanov.com>
2022-10-17 20:08:33 +0400

    fix: add text under IEEE

--- src/components/Trusted.tsx
+++ src/components/Trusted.tsx
@@ -4,14 +4,16 @@ const Trusted = () => {
   return (
     <div className='pt-8 pb-12 text-center'>
       <div className='my-12 text-3xl font-bold '>Backed by</div>
-      <div className='flex flex-row justify-evenly '>
-        <Image
-          className=''
-          src='/map/Partnerships/ieee.png'
-          width='240'
-          height='50'
-          alt=''
-        />
+      <div className='flex justify-evenly lg:flex-row '>
+        <div className=''>
+          <Image
+            className=''
+            src='/map/Partnerships/ieee.png'
+            width='240'
+            height='50'
+            alt=''
+          />
+        </div>
         <Image
           className='m-4 mt-4 rounded'
           src='/map/Partnerships/certik.png'
@@ -19,6 +21,12 @@ const Trusted = () => {
           height='60'
           alt=''
         />
+      </div>
+      <div className='flex flex-row justify-evenly '>
+        <div className='w-40 font-primary lg:w-56'>
+          Blochchain and Distributed Ledger Standards Committee
+        </div>
+        <div className='w-40 font-primary'></div>
       </div>
     </div>
   );
